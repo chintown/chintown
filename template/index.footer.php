4737104d71237ec0732fe6d8444e3fb0604fef6d
Mike Chen <chintown@gmail.com>
2014-04-06 20:38:28 +0800

    enable one-page scroll

--- template/index.footer.php
+++ template/index.footer.php
@@ -2,6 +2,7 @@
 <?php // leave file path as plain text (rather than php var) for better gen_js.py parsing
 // keep the sequence of html attribute so that parser can get correct js files
 if (DEV_MODE) { ?>
+        <script type="text/javascript" src="<?=WEB_PATH?>/js/vendor/jquery.onepage-scroll.js"></script>
 <?php } else { ?>
         <script src="<?=WEB_PATH?>/js/index.lib.min.js" type="text/javascript"></script>
 <?php } ?>
