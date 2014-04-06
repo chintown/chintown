92b54086b933e73a80bf809f7102188a4519ff9a
Mike Chen <chintown@gmail.com>
2014-04-06 16:16:39 +0800

    add index entry

---
+++ entry/index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    $path_fix_before_inc = '../';
+    require $path_fix_before_inc.'core/main.inc.php';
+    require 'template/header.php';
+    //require 'core/authentication.php';
+?>
+
+<div id="index">
+</div>
+
+<div id="wrap_template">
+<?php
+    //require 'template/crud.php';
+    //require 'template/index.crud.php';
+?>
+</div>
+<?php
+    add_extra_footer('index.footer.php');
+    require 'template/footer.php';
+?>
