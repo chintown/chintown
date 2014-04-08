5151ad81808bc23bf84b9712b4b5401caadb0c29
Mike Chen <chintown@gmail.com>
2014-04-08 14:05:10 +0800

    update control slide after page changed

--- htdoc/js/index.js
+++ htdoc/js/index.js
@@ -4,14 +4,16 @@
 function initOnePageScrolling() {
     // https://github.com/peachananr/onepage-scroll
     $("#index").onepage_scroll({
-        sectionContainer: ".page",     // sectionContainer accepts any kind of selector in case you don't want to use section
-        easing: "ease-out",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+        sectionContainer: ".page",       // sectionContainer accepts any kind of selector in case you don't want to use section
+        easing: "ease-out",              // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-        animationTime: 500,             // AnimationTime let you define how long each section takes to animate
+        animationTime: 500,              // AnimationTime let you define how long each section takes to animate
         pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-        updateURL: true,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+        updateURL: true,                 // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
         beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-        afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+        afterMove: function(index) {     // This option accepts a callback function. The function will be called after the page moves.
+            $('#page_control').val(parseInt(window.location.hash.replace('#', ''), 10))
+        },
         loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
         keyboard: true,                  // You can activate the keyboard controls
         responsiveFallback: false        // You can fallback to normal page scroll by defining the width of the browser in which
@@ -20,7 +22,9 @@ function initOnePageScrolling() {
     });
 }
 function initPageControl() {
-    $('#page_control')
+    var $controls = $('#controls'),
+        $pageControl = $('#page_control');
+    $pageControl
         .css('width', $(window).height() * 0.7)
         .attr('max', $('.page').length)
         .val(parseInt(window.location.hash.replace('#', ''), 10))
@@ -35,14 +39,14 @@ function initPageControl() {
             $(this).removeClass('active');
         })
         ;
-    $('#controls')
+    $controls
         .css({
             bottom: 0 + $(window).height() / 2,
-            right: 0 - $('#page_control').width() / 2 + 40
+            right: 0 - $pageControl.width() / 2 + 40
         })
         ;
     if (IS_MOBILE) {
-        $('#controls').show();
+        $controls.show();
         $('.onepage-pagination').hide();
     }
 }
