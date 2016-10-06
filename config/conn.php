55e093fa1dbe6df3f83938670a8c6715a97bd651
Mike Chen <chintown@gmail.com>
2016-10-05 21:39:29 -0700

    remove pw dep

---
+++ config/conn.php
@@ -0,0 +1,8 @@
+<?php
+    define('AUTH_DB',       'mypages');
+    define('AUTH_TABLE',    '');
+    require('/home/chintown/pigpen/pw.php');
+    define('AUTH_SALT',     ''); // deprecated. use random salt
+
+    define('AUTH_FIELD_USER', '');
+    define('AUTH_FIELD_PASS', '');
